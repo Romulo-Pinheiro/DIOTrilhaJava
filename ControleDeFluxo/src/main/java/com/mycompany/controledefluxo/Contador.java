ba0b93ca72e8c4fd6250e9a126403944ccb8c438
Romulo Pinheiro <romuloe.p.jesus@gmail.com>
2024-08-06 22:50:37 -0300

    Atividade Controle de Fluxo

---
+++ ControleDeFluxo/src/main/java/com/mycompany/controledefluxo/Contador.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.controledefluxo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author romulo
+ */
+
+class ParametrosInvalidosException extends Exception{
+                public ParametrosInvalidosException(String message){
+                    super(message);
+                }
+}
+
+public class Contador {
+    
+    public static void main(String[] args) {
+                Scanner terminal = new Scanner(System.in);
+                System.out.println("Digite o primeiro parâmetro");
+                int parametroUm = terminal.nextInt();
+                System.out.println("Digite o segundo parâmetro");
+                int parametroDois = terminal.nextInt();
+
+                try {
+                        //chamando o método contendo a lógica de contagem
+                        contar(parametroUm, parametroDois);
+
+                }catch (ParametrosInvalidosException exception) {
+                        System.out.println(exception);
+                }
+		
+    }
+
+                
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+                                if (parametroUm > parametroDois){
+                                        throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+                                }
+                        
+                
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+                                for (int i = 1; i <= contagem; i++){
+                                    System.out.println(i);
+                                }
+    }
+
+}
